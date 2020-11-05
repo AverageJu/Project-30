e3ab734396b5ce26adf92951127abd30382d3737
AverageJu <71568545+AverageJu@users.noreply.github.com>
2020-11-05 16:55:10 -0600

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,16 @@
+class Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/wood1.png");
+      visibility=true;
+      
+    }
+  if(boxspeed = 3){
+    display(boxspeed);
+  }
+
+          visibility=5;
+          push();
+          pop();
+  };
+  
